4fd31b494e84f4d0983bc1e7ba992fda0e582254
Samarth Kadambi <samarthk@umich.edu>
2025-08-07 22:19:52 -0500

    upping version

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ export default {
   expo: {
     name: "GoodEye",
     slug: "goodeye",
-    version: "1.0.0",
+    version: "1.0.1",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "light",
